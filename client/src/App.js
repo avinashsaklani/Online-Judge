c6a3d2a7865b1a1286b9290db27a3922ec069bb7
avinashsaklani <avinashsaklani7@gmail.com>
2023-07-30 17:40:05 +0530

    Generating Verdicts

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,12 @@ import axios from 'axios'
 
 function App() {
   const [code, setCode] = useState('');
+
   // console.log(code);
   const [output, setOutput] = useState('');
+  let ExpectedOutput = "0123456789";
+
+
 
   const handleSubmit = async () => {
     const payload = {
@@ -18,6 +22,7 @@ function App() {
       console.log(data);
       setOutput(data.output);
     } catch (error) {
+      setOutput("Error! Please re-check");
       console.log(error.message);
     }
   }
@@ -29,13 +34,33 @@ function App() {
         <select className="select-box" >
           <option value="cpp">C++</option>
         </select>
-        <textarea className="textarea" id="" cols="80" rows="20" value={code} onChange={(e) => { setCode(e.target.value); }}></textarea>
+        <textarea className="textarea" placeholder='Write your code here...' id="" cols="80" rows="20" value={code} onChange={(e) => { setCode(e.target.value); }}></textarea>
         <button onClick={handleSubmit}>Run</button>
         {output &&
-          <div className="outputBox">
-            <p>{output}</p>
-          </div>
+          <span className="verdict">
+            <br />
+            {ExpectedOutput === output.toString().split("\r\n")[0] ? "Success! All test cases Passed." : "Failed Test Cases!"}
+          </span>
         }
+        <div className='verdict-container'>
+          <div className='output-div'>
+            {output &&
+              <div className="outputBox">
+                <h3> {'Your Output : '}</h3>
+                <br />
+                <p>{'> '}{output}</p>
+              </div>
+            }
+            {output &&
+              <div className="outputBox">
+                <h3> {'Expected Output : '}</h3>
+                <br />
+                <p>{'> '}{ExpectedOutput}</p>
+              </div>
+            }
+          </div>
+
+        </div>
       </div >
     </>
   );
